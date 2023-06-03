c10a7da019254cbcbccea28e3fb4ba7a53026e82
MartinCespedes <martin.cespedes002@mymdc.net>
2023-06-03 08:06:05 -0400

    Updated pixel striker, && now ship selector goes into game canvas, edited erorr where ship was going out of vision

--- pixel-strike/src/Game.jsx
+++ pixel-strike/src/Game.jsx
@@ -1,60 +1,101 @@
 import React from 'react';
 import './Game.css';
-import RedDestroyer from './assets/Red_Destroyer.gif'
-import YellowStreak from './assets/Yellow_Streak.gif'
-import BlueLighting from './assets/Blue_Lighting.gif'
-import PurpleDestroyer from './assets/Purple_Destroyer.gif'
-// import spaceship from './assets/spaceship_svg.svg'; 
+import RedDestroyer from './assets/Red_Destroyer.gif';
+import YellowStreak from './assets/Yellow_Streak.gif';
+import BlueLighting from './assets/Blue_Lighting.gif';
+import PurpleDestroyer from './assets/Purple_Destroyer.gif';
 
 class Game extends React.Component {
   constructor(props) {
     super(props);
     this.canvasRef = React.createRef();
+    this.state = {
+      gameStarted: false,
+      selectedShipIndex: 0,
+      shipNames: [
+        { name: "Red Destroyer", color: "red", image: RedDestroyer },
+        { name: "Lighting Speed", color: "blue", image: BlueLighting },
+        { name: "Galaxy Gladiator", color: "purple", image: PurpleDestroyer },
+        { name: "Stellar Streak", color: "yellow", image: YellowStreak },
+      ],
+      shipPosition: {
+        x: 375, 
+        y: 500
+      }
+    };
   }
 
   componentDidMount() {
+    window.addEventListener("keydown", this.handleKeyDown);
+    window.addEventListener("keyup", this.handleKeyUp);
     const canvas = this.canvasRef.current;
-    const ctx = canvas.getContext('2d');
+    this.ctx = canvas.getContext('2d');
 
-    const stars = Array.from({ length: 200 }, () => ({
+    this.stars = Array.from({ length: 200 }, () => ({
       x: Math.random() * canvas.width,
       y: Math.random() * canvas.height,
       size: Math.random() * 5,
     }));
+    this.drawFrame();
+  }
 
-    const drawFrame = () => {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+  componentWillUnmount() {
+    window.removeEventListener("keydown", this.handleKeyDown);
+    window.removeEventListener("keyup", this.handleKeyUp);
+  }
 
-      stars.forEach((star, i) => {
-        ctx.beginPath();
-        ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fillStyle = '#ffffff';
-        ctx.fill();
+  drawFrame = () => {
+    const { gameStarted, shipPosition, selectedShipIndex, shipNames } = this.state;
+    if (this.ctx) {
+      this.ctx.clearRect(0, 0, 800, 600);
+
+      // Draw stars
+      this.stars.forEach((star, i) => {
+        this.ctx.beginPath();
+        this.ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
+        this.ctx.closePath();
+        this.ctx.fillStyle = '#ffffff';
+        this.ctx.fill();
 
         star.size += Math.random() > 0.5 ? 0.2 : -0.2;
         if (star.size < 0) star.size = 0;
         if (star.size > 5) star.size = 5;
       });
 
-      requestAnimationFrame(drawFrame);
-    };
+      // Draw the ship if the game has started
+      if (gameStarted) {
+        const shipImage = new Image();
+        shipImage.src = shipNames[selectedShipIndex].image;
+        this.ctx.drawImage(shipImage, shipPosition.x, shipPosition.y, 50, 50);
+      }
 
-    drawFrame();
-  }
+      requestAnimationFrame(this.drawFrame);
+    }
+  };
 
+  handleKeyDown = (event) => {
+    const { shipPosition } = this.state;
+    switch(event.key) {
+      case "ArrowUp":
+        this.setState({ shipPosition: {...shipPosition, y: shipPosition.y - 50 }});
+        break;
+      case "ArrowDown":
+        this.setState({ shipPosition: {...shipPosition, y: shipPosition.y + 50 }});
+        break;
+      case "ArrowRight":
+        this.setState({ shipPosition: {...shipPosition, x: shipPosition.x + 50}});
+        break;
+      case "ArrowLeft":
+        this.setState({ shipPosition: {...shipPosition, x: shipPosition.x - 50 }});
+        break;
+      default:
+        break;
+    }
+  };
 
-  
-  
-  state = {
-    selectedShipIndex: 0,
-    shipNames: [
-      { name: "Red Destroyer", color: "red", image: RedDestroyer },
-      { name: "Lighting Speed", color: "blue", image: BlueLighting },
-      { name: "Galaxy Gladiator", color: "purple", image: PurpleDestroyer },
-      { name: "Stellar Streak", color: "yellow", image: YellowStreak },
-    ],
-  }
+  handleKeyUp = (event) => {
+    // If needed, you can handle key up events here
+  };
 
   handleShipChange = (direction) => {
     const { selectedShipIndex, shipNames } = this.state;
@@ -65,10 +106,14 @@ class Game extends React.Component {
     else if (newIndex >= shipNames.length) newIndex = 0;
 
     this.setState({ selectedShipIndex: newIndex });
-  }
+  };
+
+  handleGameStart = () => {
+    this.setState({ gameStarted: true });
+  };
 
   render() {
-    const { selectedShipIndex, shipNames } = this.state;
+    const { gameStarted, selectedShipIndex, shipNames } = this.state;
     return (
       <div id="layout-container">
         <div id="info-container">
@@ -80,12 +125,13 @@ class Game extends React.Component {
             <h2>Lives: <span id="lives">3</span></h2>
           </div>
           <div id="ship-selector">
-            <button className="arrow-btn" onMouseDown={() => this.setState({leftPressed: true})} onMouseUp={() => this.setState({leftPressed: false})} onClick={() => this.handleShipChange(-1)}>&#9664;</button>
+            <button className="arrow-btn" onClick={() => this.handleShipChange(-1)}>&#9664;</button>
             <div className="ship-display">
-          <img src={shipNames[selectedShipIndex].image} alt="ship"/> {/* This will now display the image of the selected ship */}
-          <span className={`ship-name ${shipNames[selectedShipIndex].color}`}>{shipNames[selectedShipIndex].name}</span>
-        </div>
-            <button className="arrow-btn" onMouseDown={() => this.setState({rightPressed: true})} onMouseUp={() => this.setState({rightPressed: false})} onClick={() => this.handleShipChange(1)}>&#9654;</button>
+              <img src={shipNames[selectedShipIndex].image} alt="ship"/>
+              <span className={`ship-name ${shipNames[selectedShipIndex].color}`}>{shipNames[selectedShipIndex].name}</span>
+            </div>
+            <button className="arrow-btn" onClick={() => this.handleShipChange(1)}>&#9654;</button>
+            {!gameStarted && <button onClick={this.handleGameStart}>Start Game</button>}
           </div>
         </div>
         <div id="game-container">
